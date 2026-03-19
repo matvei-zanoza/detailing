3a3fca6a231dc4342a269f09ac071d5a004d1d33
matvei-zanoza <matvei@avenkara.ai>
2026-03-19 15:06:38 +0700

    Expose logo.png rewrite

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,12 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  async rewrites() {
+    return [
+      { source: "/logo.png", destination: "/images/logo.png" },
+      { source: "/logo-v2.png", destination: "/images/logo-v2.png" },
+    ];
+  },
 };
 
 export default nextConfig;
